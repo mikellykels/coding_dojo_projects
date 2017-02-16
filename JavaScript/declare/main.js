61bdfcba6c0aacbbbe422bf8dd466172222aea42
Mikaela Carino <mikaela.carino@gmail.com>
2017-02-15 18:15:46 -0800

    added JS directory and JS assignment

---
+++ JavaScript/declare/main.js
@@ -0,0 +1,31 @@
+// 4 Numbers
+// 3 Strings
+// 2 Booleans
+// 1 Undefined Variable
+
+var num1 = 3;
+var num2 = 0.5;
+var num3 = -5;
+var num4 = 10000;
+
+var string1 = "Hello";
+var string2 = "World";
+var string3 = "JavaScript";
+
+var bool1 = true;
+var bool2 = false;
+var und
+
+console.log("Number 1: " + num1);
+console.log("Number 2: " + num2);
+console.log("Number 3: " + num3);
+console.log("Number 4: " + num4);
+
+console.log("String 1: " + string1);
+console.log("String 2: " + string2);
+console.log("String 3: " + string3);
+
+console.log("True or False? " + bool1);
+console.log("True or False? " + bool2);
+
+console.log("This var is: " + und);
