ac3663dac2bccfb67d43cf5549a2eab5c58b9d2d
Mikaela Carino <mikaela.carino@gmail.com>
2017-02-21 12:27:49 -0800

    added python directory and try_python assignment

---
+++ Python/try_python/trypython.py
@@ -0,0 +1,12 @@
+# Create a variable with any name you like and assign it any value.
+bicycle = "2 wheels"
+print bicycle
+# Reassign a new value to the same variable.
+bicycle = "fixed"
+print bicycle
+# Print a mathematical operation.
+print 3*5
+# Add two strings together.
+print "2 wheels" + " fixed"
+# Add a string and an integer.
+print "2 wheels", 5
