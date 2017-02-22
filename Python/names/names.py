34487da302e8ad618e827265e2be787fc968f943
Mikaela Carino <mikaela.carino@gmail.com>
2017-02-21 19:00:53 -0800

    added names assignment

---
+++ Python/names/names.py
@@ -0,0 +1,86 @@
+'''
+Part I:
+
+Given the following list:
+
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+Create a program that outputs:
+
+Michael Jordan
+John Rosales
+Mark Guillen
+KB Tonel
+
+'''
+
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+def names():
+    for name in students:
+        print name["first_name"] + " " + name["last_name"]
+
+names()
+
+'''
+Part II:
+
+Now, given the following dictionary:
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+Create a program that prints the following format (including number of characters in each combined name):
+
+Students
+1 - MICHAEL JORDAN - 13
+2 - JOHN ROSALES - 11
+3 - MARK GUILLEN - 11
+4 - KB TONEL - 7
+Instructors
+1 - MICHAEL CHOI - 11
+2 - MARTIN PURYEAR - 13
+'''
+
+users = {
+    'Students': [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ],
+    'Instructors': [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+    ]
+}
+
+def names2():
+    count = 0
+    for key, data in users.items():
+        print key
+        for value in data:
+            count += 1
+            print count, "- " + value['first_name'] + " " + value['last_name'] + " -", (len(value['first_name'] + " " + value['last_name'])-1)
+
+names2()
